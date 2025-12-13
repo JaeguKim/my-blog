e9175345fa3cf629dbc1c15a875eca652f170883
jaegoo-kim <jaegoo.kim@overdare.com>
2025-12-13 19:11:13 +0900

    add content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jacob Kim';
+export const SITE_DESCRIPTION = 'Backend Software Engineer - Building scalable systems with Go, TypeScript, and cloud technologies';
